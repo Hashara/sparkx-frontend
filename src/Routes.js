e402f53ea80b0d55d630348cdd5786176f384ef0
Hashara <kumarasinghe.k.a.s.h@gmail.com>
2021-06-15 16:26:54 +0530

    add auth pages

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import App from './App';
+import SignIn from "./views/Auth/pages/Signin";
+import Register from "./views/Auth/pages/Register";
+import SignUp from "./views/Auth/pages/SignUp";
 
 const Routes = () =>{
     return(
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component = {App} />
+                <Route path= "/signin" exact component={SignIn}/>
+                <Route path= "/register" exact component={Register} />
+                <Route path= "/signup" exact component={SignUp} />
             </Switch>
         </BrowserRouter>
     )
--- src/views/home/components/SymptomCard.js
+++ src/views/home/components/SymptomCard.js
@@ -4,11 +4,11 @@ import CardContent from "@material-ui/core/CardContent";
 import Box from "@material-ui/core/Box";
 import {makeStyles} from "@material-ui/core/styles";
 import {CardMedia} from "@material-ui/core";
-import symptoms from "../../../asserts/symptoms.jpg"
-import ButtonComponent from "./ButtonComponent";
+import symptoms from "../../../asserts/symptoms.jpg";
 import Grid from "@material-ui/core/Grid";
 import '../../../index.css';
 import Button from "@material-ui/core/Button";
+import {Link,withRouter} from 'react-router-dom';
 
 const useStyles = makeStyles({
     root: {
@@ -34,35 +34,35 @@ const useStyles = makeStyles({
 const SymptomCard = () => {
     const classes = useStyles();
 
-    return(
+    return (
         <Box p={1} bgcolor="background.paper">
-            <Card className={classes.root}  variant="outlined" >
+            <Card className={classes.root} variant="outlined">
                 <CardContent>
-                    <Grid container justify="center"  p={10}>
+                    <Grid container justify="center" p={10}>
                         <h1>Having COVID-19 symptoms???</h1>
                     </Grid>
                 </CardContent>
                 <CardMedia>
-                        <Grid container className={classes.root}>
-                            <Grid item xs={12}>
-                                <Grid container justify="center" spacing={2}>
-                                        <Grid item>
-                                            <Box p={1} my={0.5} mx={0.5} >
-                                                <img src={symptoms} alt="symptoms" className='symptoms'/>
-                                            </Box>
-                                        </Grid>
+                    <Grid container className={classes.root}>
+                        <Grid item xs={12}>
+                            <Grid container justify="center" spacing={2}>
+                                <Grid item>
+                                    <Box p={1} my={0.5} mx={0.5}>
+                                        <img src={symptoms} alt="symptoms" className='symptoms'/>
+                                    </Box>
                                 </Grid>
                             </Grid>
                         </Grid>
+                    </Grid>
                 </CardMedia>
-                <Grid container justify="center"  p={10}>
+                <Grid container justify="center" p={10}>
                     <Box m={2} pt={3}>
-                        <Button variant="contained" color="primary">
+                        <Button component={ Link } to="/register" variant="contained" color="primary">
                             Register
                         </Button>
                     </Box>
                     <Box m={2} pt={3}>
-                        <Button variant="contained" color="primary">
+                        <Button component={ Link } to="/signin"  variant="contained" color="primary">
                             SIGN IN
                         </Button>
                     </Box>
